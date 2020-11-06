cee1128ec6389b89e0b114a0432da092ce406171
Dodger-42 <kevin.bing.barber@gmail.com>
2020-11-05 18:27:56 -0800

    index, seeds, data, router, model

--- migrations/20201105172949_food.js
+++ migrations/20201105172949_food.js
@@ -1,8 +1,12 @@
 
-exports.up = function(knex) {
+exports.up = async function(knex) {
+    await knex.schema.createTable('food', tble => {
+        tble.increments()
+        tble.text('name').notNullable()
+        tble.text('type').notNullable()
+    })
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('food')
+  };
